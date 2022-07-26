89391570aef2ade1b730ad04cbcacd073bfffdf6
Leon Vallejos <dev.leonvallejos@gmail.com>
2022-07-26 05:22:01 -0300

    Set api geolocation

---
+++ script.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const OPTIONS = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '3f528120e9msh55db7696d2bc256p1e3765jsna12e13206f9b',
+    'X-RapidAPI-Host': 'ip-geolocation-and-threat-detection.p.rapidapi.com'
+  }
+};
+
+// url: 'https://ip-geolocation-and-threat-detection.p.rapidapi.com/54.85.132.205'
